54112f185f64933958ac1c03cec349d612ccd122
Julio Cesar <jc.juninho@hotmail.com>
2016-06-02 17:10:02 -0300

    first commit

---
+++ refugiados/bookmarklet.js
@@ -0,0 +1,49 @@
+(function(){
+
+	var fixElement = function(elem){
+		var childs = elem.childNodes;
+		if(childs.length < 2000){
+			for(var i = 0, max = childs.length; i < max; i++){
+				var child = childs[i];
+				if(child.nodeType == 3 && child.nodeValue.match(/(refugiados)/i)){
+					child.nodeValue = child.nodeValue.replace(/(refugiados)/i, "Seres Humanos");
+				}
+				if(child.nodeType == 3 && child.nodeValue.match(/(refugiado)/i)){
+					child.nodeValue = child.nodeValue.replace(/(refugiado)/i, "Ser Humano");
+				}
+				if(child.nodeType == 3 && child.nodeValue.match(/(refugee)/i)){
+					child.nodeValue = child.nodeValue.replace(/(refugee)/i, "Human Being");
+				}
+				if(child.nodeType == 3 && child.nodeValue.match(/(flüchtlinge)/i)){
+					child.nodeValue = child.nodeValue.replace(/(flüchtlinge)/i, "Menschliches Wesen");
+				}
+				if(child.nodeType == 3 && child.nodeValue.match(/(flüchtling)/i)){
+					child.nodeValue = child.nodeValue.replace(/(flüchtling)/i, "Menschliche Wesen");
+				}
+				if(child.nodeType == 3 && child.nodeValue.match(/(réfugiés)/i)){
+					child.nodeValue = child.nodeValue.replace(/(réfugiés)/i, "Êtres Humains");
+				}
+				if(child.nodeType == 3 && child.nodeValue.match(/(réfugié)/i)){
+					child.nodeValue = child.nodeValue.replace(/(réfugié)/i, "Être Humain");
+				}
+				if(child.childNodes.length > 0 && child.tagName !== "SCRIPT" && child.tagName !== "STYLE"){
+					fixElement(child);
+				}
+			}
+		}
+	}
+
+	var fixWords = function(){
+		var head = document.getElementsByTagName("head")[0];
+		var body = document.getElementsByTagName("body")[0];
+
+		fixElement(head);
+		fixElement(body);
+	}
+
+	document.onreadystatechange = function(){
+	    if (document.readyState === 'complete') fixWords();
+	}
+	window.onload = fixWords;
+	
+})();
